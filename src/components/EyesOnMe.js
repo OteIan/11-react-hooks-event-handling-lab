476c2225a01f2c52c6174d6cab053242347b6177
OteIan <oteian9@gmail.com>
2024-08-12 15:28:39 +0300

    event-handling done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    return (
+        <button
+            onFocus={(_e) => console.log('Good!')}
+            onBlur={(_e) => console.log('Hey! Eyes on me!')}
+        >Eyes on me</button>
+    );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,12 @@
 // Code Keypad Component Here
+import React from "react"
 
 function Keypad (){
     return (
-        <div></div>
+        <input
+            type="password"
+            onChange={(_e) => console.log('Entering password...')}
+        ></input>
     )
 }
 
